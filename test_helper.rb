a043e56bf37f44851593547025ecec078ed32c22
Audra Matar <audramatar@gmail.com>
2017-12-06 19:30:18 -0600

    trying simplecov instead

--- test_helper.rb
+++ test_helper.rb
@@ -1,4 +1,7 @@
-require 'coveralls'            # These two lines must go first
-Coveralls.wear!
+#require 'coveralls'            # These two lines must go first
+#Coveralls.wear!
+
+require 'simplecov'            # These two lines must go first
+SimpleCov.start
 
 require 'minitest/autorun'
