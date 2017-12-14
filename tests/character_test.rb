2cdd82db7098625cbfaad2ee9ee03e9b43adf8a0
Audra Matar <audramatar@gmail.com>
2017-12-14 11:01:08 -0600

    Fixed rubocop stuff in tests.

--- tests/character_test.rb
+++ tests/character_test.rb
@@ -1,46 +1,70 @@
 require_relative '../test_helper.rb'
-require_relative "../lib/characters/test_character.rb"
+require_relative '../lib/characters/test_character.rb'
 require 'byebug'
 
+# CharacterTest just tests to ensure that all of the
+# attributes expected of a character are there and not nil.
+# This includes objects that the character should be able
+# to create, like attack and defence objects.
 class CharacterTest < MiniTest::Test
+  @character = TestCharacter.new('Test Character')
 
-  @character = TestCharacter.new("Test Character")
+  not_nil = { 'name' => @character.name, 'level' => @character.level,
+              'ac' => @character.ac, 'bab' => @character.bab,
+              'init' => @character.init, 'hp' => @character.hp,
+              'max_hp' => @character.max_hp, 'str' => @character.str,
+              'dex' => @character.dex, 'mana' => @character.mana,
+              'max_mana' => @character.max_mana, 'cbm' => @character.cbm,
+              'mag resist' => @character.mag_resist, 'con' => @character.con,
+              'cbm def' => @character.cbm_def, 'mag' => @character.mag,
+              'shield_bonus' => @character.shield_bonus,
+              'armor bonus' => @character.armor_bonus,
+              'spell_failure_chance' => @character.spell_failure_chance,
+              'equipped weapon' => @character.equipped_weapon,
+              'cha' => @character.cha, 'str_mod' => @character.str_modifier,
+              'dex_mod' => @character.dex_modifier,
+              'con_mod' => @character.con_modifier,
+              'mag_mod' => @character.mag_modifier,
+              'cha_mod' => @character.cha_modifier,
+              'attack' => @character.attack, 'damage' => @character.damage,
+              'one_hand_atk' => @character.one_hand_atk,
+              'dual_wield_atk' => @character.dual_wield_atk,
+              'two_hand_atk' => @character.two_hand_atk,
+              'unarmed_atk' => @character.unarmed_atk,
+              'staff_atk' => @character.staff_atk,
+              'one_hand_damage' => @character.one_hand_damage,
+              'dual_wield_damage' => @character.dual_wield_damage,
+              'unarmed_damage' => @character.unarmed_damage,
+              'two_hand_damage' => @character.two_hand_damage,
+              'staff_damage' => @character.staff_damage,
+              'one_hand_prof' => @character.one_hand_prof,
+              'two_hand_prof' => @character.two_hand_prof,
+              'dual_wield_prof' => @character.dual_wield_prof,
+              'unarmed_prof' => @character.unarmed_prof,
+              'magic_prof' => @character.magic_prof,
+              'dead' => @character.dead, 'party' => @character.party,
+              'inventory' => @character.inventory }
 
-  should_not_be_nil = {"name" => @character.name, "level" => @character.level, "ac" => @character.ac, "bab" => @character.bab, "init" => @character.init,
-                       "hp" => @character.hp, "max_hp" => @character.max_hp, "str" => @character.str, "dex" => @character.dex, 
-                       "mana" => @character.mana, "max_mana" => @character.max_mana, "mag resist" => @character.mag_resist, "cbm" => @character.cbm,
-                       "cbm def" => @character.cbm_def, "shield_bonus" => @character.shield_bonus, "armor bonus" => @character.armor_bonus,
-                       "spell_failure_chance" => @character.spell_failure_chance, "equipped weapon" => @character.equipped_weapon, 
-                       "con" => @character.con, "mag" => @character.mag, "cha" => @character.cha, "str_modifier" => @character.str_modifier, 
-                       "dex_modifier" => @character.dex_modifier, "con_modifier" => @character.con_modifier, 
-                       "mag_modifier" => @character.mag_modifier, "cha_modifier" => @character.cha_modifier, 
-                       "attack" => @character.attack, "one_hand_atk" => @character.one_hand_atk, "dual_wield_atk" => @character.dual_wield_atk, 
-                       "two_hand_atk" => @character.two_hand_atk, "unarmed_atk" => @character.unarmed_atk, "staff_atk" => @character.staff_atk, 
-                       "damage" => @character.damage, "one_hand_damage" => @character.one_hand_damage, 
-                       "dual_wield_damage" => @character.dual_wield_damage, "unarmed_damage" =>@character.unarmed_damage, 
-                       "two_hand_damage" => @character.two_hand_damage, "staff_damage" => @character.staff_damage, 
-                       "one_hand_prof" => @character.one_hand_prof, "two_hand_prof" => @character.two_hand_prof, 
-                       "dual_wield_prof" => @character.dual_wield_prof, "unarmed_prof" => @character.unarmed_prof, "magic_prof" => @character.magic_prof,
-                       "dead" => @character.dead, "party" => @character.party, "inventory" => @character.inventory}
-
-  should_not_be_nil.each_pair do |name, attribute|
+  not_nil.each_pair do |name, attribute|
     define_method("test_#{name}_does_not_return_nill") do
       refute_nil(attribute)
     end
   end
 
-  atk_obj_attributes = {"attack roll" => @character.create_attack_object([@character])[:attack], "damage" => @character.create_attack_object([@character])[:damage],
-                        "target" => @character.create_attack_object([@character])[:target], "message" => @character.create_attack_object([@character])[:message]}
+  atk_obj = { 'atk roll' => @character.create_attack_object([@character])[:attack],
+              'damage' => @character.create_attack_object([@character])[:damage],
+              'target' => @character.create_attack_object([@character])[:target],
+              'message' => @character.create_attack_object([@character])[:message] }
 
-  atk_obj_attributes.each_pair do |name, attribute|
+  atk_obj.each_pair do |name, attribute|
     define_method("test_#{name}_exists_in_attack_object") do
       refute_nil(attribute)
     end
   end
-  test_attack_object = {attack: [1], damage: [1]}
-  defence_obj_attributes = {"message" => @character.defend(test_attack_object)[:message]}
+  test_attack_object = { attack: [1], damage: [1] }
+  defence_obj = { 'message' => @character.defend(test_attack_object)[:message] }
 
-  defence_obj_attributes.each_pair do |name, attribute|
+  defence_obj.each_pair do |name, attribute|
     define_method("test_#{name}_exists_in_defence_object") do
       refute_nil(attribute)
     end
--- tests/combat_test.rb
+++ tests/combat_test.rb
@@ -1,16 +1,16 @@
 require_relative '../test_helper.rb'
-require_relative "../lib/character.rb"
-require_relative "../lib/combat.rb"
+require_relative '../lib/character.rb'
+require_relative '../lib/combat.rb'
 require 'byebug'
 
+# Testing the base of the combat class.
 class CombatTest < MiniTest::Test
-  # combat between characters until one side is dead.
-  @c1 = Character.new("Ally 1")
-  @c2 = Character.new("Ally 2")
-  @c3 = Character.new("Ally 3")
-  @e1 = Character.new("Enemy 1")
-  @e2 = Character.new("Enemy 2")
-  @e3 = Character.new("Enemy 3")
+  @c1 = Character.new('Ally 1')
+  @c2 = Character.new('Ally 2')
+  @c3 = Character.new('Ally 3')
+  @e1 = Character.new('Enemy 1')
+  @e2 = Character.new('Enemy 2')
+  @e3 = Character.new('Enemy 3')
 
   @c1.dead = true
   @e1.dead = true
@@ -19,14 +19,18 @@ class CombatTest < MiniTest::Test
   combat3 = Combat.new([@c1], [@e2, @e3])
   combat4 = Combat.new([@c2, @c3], [@e1])
 
-  scenarios_to_test = {"enemies_win" => [combat,  "All allies have died! Enemies win!"],
-                       "allies_win" =>  [combat2, "All enemies have died! Allies win!"],
-                       "multiple_enemies_win" => [combat3,  "All allies have died! Enemies win!"],
-                       "multiple_allies_win"  => [combat4,  "All enemies have died! Allies win!"]}
+  ally_victory = 'All enemies have died! Allies win!'
+  enemy_victory = 'All allies have died! Enemies win!'
 
-  scenarios_to_test.each_pair do |name, attributes|                  
-      define_method("test_combat_completes_when_#{name}") do
-      capture_stdout {attributes[0].fight}
+  battles_to_test = { 'enemies_win' => [combat, enemy_victory],
+                      'allies_win' => [combat2, ally_victory],
+                      'multiple_enemies_win' => [combat3, enemy_victory],
+                      'multiple_allies_win' => [combat4, ally_victory] }
+
+  # combat between characters until one side is dead.
+  battles_to_test.each_pair do |name, attributes|
+    define_method("test_combat_completes_when_#{name}") do
+      capture_stdout { attributes[0].fight }
       assert(attributes[0].battle_log.include?(attributes[1]))
     end
   end
