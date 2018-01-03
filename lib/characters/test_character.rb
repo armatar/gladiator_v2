924920e410c9b46d67031f127da214fcd27b8375
Audra Matar <audramatar@gmail.com>
2018-01-03 12:06:23 -0600

    trying to add more test coverage for the character factory.

--- lib/characters/test_character.rb
+++ lib/characters/test_character.rb
@@ -3,7 +3,7 @@ require_relative '../character.rb'
 # Child class of character used specifically for testing.
 class TestCharacter < Character
   attr_accessor :max_mana, :max_hp, :ac, :attack, :damage, :mag_resist, :spell_failure_chance
-  attr_accessor :dex, :str, :con, :mag, :cha
+  attr_accessor :dex, :str, :con, :mag, :cha, :mag_modifier, :cha_modifier, :known_spells
 
   def initialize(name)
     super(name)
--- tests/character_factory_test.rb
+++ tests/character_factory_test.rb
@@ -1,5 +1,6 @@
 require_relative '../test_helper.rb'
 require_relative '../lib/factories/random_character_factory.rb'
+require_relative '../lib/characters/test_character.rb'
 require 'byebug'
 
 # Testing the random character factory
@@ -28,6 +29,35 @@ class CharacterFactoryTest < MiniTest::Test
     define_method("test_#{points_to_assign}_returns_#{total}_attribute_points") do
       att_hash = RandomCharacterFactory.randomize_attributes(points_to_assign, static_hash)
       assert_equal(total, att_hash.values.reduce(:+))
+    end
+  end
+
+  character = TestCharacter.new('Test')
+  character = RandomCharacterFactory.give_inventory('1-hand', character)
+  weapon_subtypes = ['1-hand', '2-hand', 'dual wield', 'unarmed', 'staff']
+
+  weapon_subtypes.each do |type|
+    define_method("test_that_equipped_weapon_is_of_subtype_#{type}") do
+      RandomCharacterFactory.give_inventory(type, character)
+      assert_equal(type, character.equipped_weapon[:subtype])
+    end
+  end
+
+  def test_when_one_hand_skill_inventory_has_shield
+    character2 = TestCharacter.new('Test')
+    RandomCharacterFactory.give_inventory('1-hand', character2)
+    assert(character2.inventory.values.find { |x| x[:type] == 'shield' })
+  end
+
+  spells_should_have = { [1, 1] => 4, [0, 1] => 2, [0, 0] => 0 }
+  character3 = TestCharacter.new('Test')
+
+  spells_should_have.each do |values, num_spells|
+    define_method("test_character_has_#{num_spells}_when_mag_mod_and_cha_mod_are_#{values}") do
+      character3.known_spells = {}
+      character3.mag_modifier = values[0]
+      character3.cha_modifier = values[1]
+      assert_equal(num_spells, RandomCharacterFactory.give_spells(character3).known_spells.length)
     end
   end
 end
