3d04d7515d28a5a75990d5b8100df7187386898e
Audra Matar <audramatar@gmail.com>
2018-01-03 11:36:03 -0600

    Removing spells that are not currently tested.

--- lib/spells.rb
+++ lib/spells.rb
@@ -20,14 +20,14 @@ class Spells
   end
 
   def self.buff_spells
-    yml_file.to_a.find_all { |x| x[1][:type] == 'buff' }.to_h
+    # yml_file.to_a.find_all { |x| x[1][:type] == 'buff' }.to_h
   end
 
   def self.create_curse_spells
-    yml_file.to_a.find_all { |x| x[1][:type] == 'curse' }.to_h
+    # yml_file.to_a.find_all { |x| x[1][:type] == 'curse' }.to_h
   end
 
   def self.hybrid_spells
-    yml_file.to_a.find_all { |x| x[1][:type] == 'hybrid' }.to_h
+    # yml_file.to_a.find_all { |x| x[1][:type] == 'hybrid' }.to_h
   end
 end
