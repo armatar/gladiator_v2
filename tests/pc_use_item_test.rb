c1d8c757e8ddf9b63a7d394e846433a065870b4c
Audra Matar <audramatar@gmail.com>
2018-01-05 16:37:47 -0600

    Added another test for pc use item.

--- tests/pc_use_item_test.rb
+++ tests/pc_use_item_test.rb
@@ -6,11 +6,13 @@ require_relative '../lib/characters/player_characters/test_player_character.rb'
 # A test for player character item use.
 class PCUseItemTest < MiniTest::Test
   pc = TestPlayerCharacter.new('Test')
+  ally = TestPlayerCharacter.new('Ally')
+  pc.join_party(ally.party)
   pc.inventory['small_health_potion'] = Items.consumables['small health potion']
   pc.inventory['large_health_potion'] = Items.consumables['large health potion']
 
   pc.inventory.each_pair do |name, item|
-    define_method("test_use_healing_item_#{name}") do
+    define_method("test_use_healing_item_#{name}_on_self") do
       with_stdin do |user|
         user.puts name
         user.puts 'Test'
@@ -19,6 +21,20 @@ class PCUseItemTest < MiniTest::Test
         result = 1 + item[:bonus]
         capture_stdout { pc.choose_item_to_use }
         assert_equal(result, pc.hp)
+      end
+    end
+  end
+
+  pc.inventory.each_pair do |name, item|
+    define_method("test_use_healing_item_#{name}_on_ally") do
+      with_stdin do |user|
+        user.puts name
+        user.puts 'Ally'
+        ally.max_hp = 100
+        ally.hp = 1
+        result = 1 + item[:bonus]
+        capture_stdout { pc.choose_item_to_use }
+        assert_equal(result, ally.hp)
       end
     end
   end
